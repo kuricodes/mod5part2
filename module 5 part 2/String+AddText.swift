8ab62c1899e82527e21bd9b29069100bf3f1d827
kuricodes <85655275+kuricodes@users.noreply.github.com>
2023-10-01 23:01:25 -0400

    the

---
+++ module 5 part 2/String+AddText.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension String {
+  mutating func add(text: String?, separatedBy separator: String = "")
+  {
+    if let text = text {
+      if !isEmpty {
+        self += separator
+      }
+      self += text
+    }
+  }
+}
